a5064c9ff28780fe97586fac5f3d5c064a3005a6
jasontgarwood <jasontgarwood@gmail.com>
2024-02-01 22:20:14 -0500

    update to include other weather features

--- getWeatherData.py
+++ getWeatherData.py
@@ -18,12 +18,14 @@ def getWeather(apiKey, city):
 
         #Extract weather information
         main_weather = data['weather'][0]['main']
-        return main_weather
+        temperature = data['main']['temp']
+        return main_weather, temperature 
     
     except Exception as e:
         print(f"Error: {e}") #fancy way to print error messages
         return None
-    
+
+def defineWeather()
 
 ################# Main Function ####################
 if __name__ == "__main__":
@@ -32,11 +34,20 @@ if __name__ == "__main__":
     city = 'Los Angeles' #Update this with the city!!!
     weather = getWeather(api_key, city)
 
-    if weather:
+    if weather is not None and temperature is not None:
         if weather.lower() == 'rain':
             print("It is raining.")
+            ##SQUIRT WATER
+        elif temperature < 32:  # Below freezing
+            print("It is cold.")
+            ##EXTEND SERVO
+        elif temperature > 80:  # Above 80 degrees
+            print("It is warm.")
+            ##SHRINK SERVO
         else:
-            print(weather)
-            print("It is not raining")
+            print(f"The weather is {weather} and the temperature is {temperature} degrees.")
     else:
         print("Failed to fetch weather data.")
+
+
+#
