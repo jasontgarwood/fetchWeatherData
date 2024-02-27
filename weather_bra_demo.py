4091d0caace8e684eb6fa455e775e28199ac4e0a
Jason Garwood <jasontgarwood@gmail.com>
2024-02-27 04:02:24 +0000

    Update weather_bra_demo.py

--- weather_bra_demo.py
+++ weather_bra_demo.py
@@ -1,90 +1,57 @@
-################# import Libs ####################
 import RPi.GPIO as GPIO
 import time
-import threading
+import keyboard  # Keyboard module for capturing key presses
 
-################# Init Servos ####################
-servo1_pin = 17 #
-servo2_pin = 18 #
+# GPIO pins for the servos
+servo1_pin = 18
+servo2_pin = 17
 
-# Setup GPIO
+# Set up GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo1_pin, GPIO.OUT)
 GPIO.setup(servo2_pin, GPIO.OUT)
 
-# Setup and start PWM
+# Create PWM instances for the servos
 servo1_pwm = GPIO.PWM(servo1_pin, 50)  # 50 Hz frequency
 servo2_pwm = GPIO.PWM(servo2_pin, 50)
-servo1_pwm.start(0)
-servo2_pwm.start(0)
 
-# Initial servo angles
-servo1_angle = 0
-servo2_angle = 0
-
-################# Helper Functions ####################
-# Function to set servo angle
-def set_angle(pwm, angle):
-    duty = angle / 18 + 2
-    GPIO.output(pwm, True)
-    pwm.ChangeDutyCycle(duty)
-    time.sleep(1)
-    GPIO.output(pwm, False)
-    pwm.ChangeDutyCycle(0)
-
-# Function to toggle servo state
-def toggle_servo(servo_pwm, servo_angle, active_angle, is_slow=False):
-    global servo1_pwm, servo2_pwm, servo1_angle, servo2_angle
-    if servo_angle == 0:
-        if is_slow:
-            for angle in range(0, active_angle + 1, 10):
-                set_angle(servo_pwm, angle)
-                time.sleep(0.1)
-        else:
-            set_angle(servo_pwm, active_angle)
-        servo_angle = active_angle
+# Function to move servo to a specific angle
+def move_servo(pwm, angle, slow=False):
+    duty_cycle = (angle / 18) + 2
+    pwm.start(duty_cycle)
+    if slow:
+        time.sleep(0.5)  # Adjust this value for slower movement
     else:
-        if is_slow:
-            for angle in range(active_angle, 0, -10):
-                set_angle(servo_pwm, angle)
-                time.sleep(0.1)
-        else:
-            set_angle(servo_pwm, 0)
-        servo_angle = 0
-    return servo_angle
+        time.sleep(0.1)  # Default movement speed
+    pwm.stop()
 
-# Function to handle Enter key press
-def on_enter_press():
-    global servo1_pwm, servo1_angle, servo2_pwm, servo2_angle
-    servo1_angle = toggle_servo(servo1_pwm, servo1_angle, 160)
-    servo2_angle = toggle_servo(servo2_pwm, servo2_angle, 160)
+# Main function to control the servos
+def main():
+    servo_states = [0, 160]  # Neutral and Active states for the servos
+    current_state_index = 0  # Index to keep track of current state
 
-# Function to handle Space key press
-def on_space_press():
-    global servo1_pwm, servo1_angle, servo2_pwm, servo2_angle
-    servo1_angle = toggle_servo(servo1_pwm, servo1_angle, 160, is_slow=True)
-    servo2_angle = toggle_servo(servo2_pwm, servo2_angle, 160, is_slow=True)
+    try:
+        while True:
+            if keyboard.is_pressed('f'):
+                current_state_index = (current_state_index + 1) % len(servo_states)
+                move_servo(servo1_pwm, servo_states[current_state_index])
+                move_servo(servo2_pwm, servo_states[current_state_index])
+                print("Servos toggled to state:", current_state_index)
 
-# Keyboard input thread
-def key_input_thread():
-    while True:
-        key = input()
-        if key == '':
-            on_enter_press()
-        elif key == ' ':
-            on_space_press()
+            elif keyboard.is_pressed('s'):
+                current_state_index = (current_state_index + 1) % len(servo_states)
+                move_servo(servo1_pwm, servo_states[current_state_index], slow=True)
+                move_servo(servo2_pwm, servo_states[current_state_index], slow=True)
+                print("Servos toggled to state with slow movement:", current_state_index)
 
-# Start keyboard input thread
-input_thread = threading.Thread(target=key_input_thread)
-input_thread.daemon = True
-input_thread.start()
+    except KeyboardInterrupt:
+        pass
 
-try:
-    while True:
-        # Keep the main thread running
-        time.sleep(1)
+    finally:
+        # Clean up GPIO
+        servo1_pwm.stop()
+        servo2_pwm.stop()
+        GPIO.cleanup()
 
-except KeyboardInterrupt:
-    servo1_pwm.stop()
-    servo2_pwm.stop()
-    GPIO.cleanup()
+if __name__ == "__main__":
+    main()
